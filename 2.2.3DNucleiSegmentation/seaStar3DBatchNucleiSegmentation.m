b757d40655ab32a8defc592382907f5afda644a7
Miguel Ángel Spínola <98082525+Spinola1014@users.noreply.github.com>
2022-08-25 11:53:36 +0200

    Create seaStar3DBatchNucleiSegmentation.m

---
+++ 2.2.3DNucleiSegmentation/seaStar3DBatchNucleiSegmentation.m
@@ -0,0 +1,16 @@
+clc;
+clearvars;
+workspace;
+imtool close all;
+format long g;
+format compact;
+
+directory = dir(strcat("./originalNuclei/", '*.tif'));
+fromPath = "./originalNuclei/";
+savePath = "./segmentedNuclei/";
+
+for imageIx = 1:size(directory, 1)
+    name = directory(imageIx).name;
+    SeaStar3DNucleiSegmentation(fromPath, name, savePath, strcat('segmented_nuclei_', name), 1);
+    disp(imageIx);
+end
