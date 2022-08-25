73a5402d8fa81f5794d8301ba6d48b900e3a5551
Miguel Ángel Spínola <98082525+Spinola1014@users.noreply.github.com>
2022-08-25 18:59:10 +0200

    Delete 2.Read&WriteTiff directory

--- 2.Read&WriteTiff/readStackTiff.m
+++
@@ -1,8 +0,0 @@
-function [loadedImage, infoImage] = readStackTif(fileName)
-
-    infoImage = imfinfo(fileName);
-    loadedImage = uint8(zeros(infoImage(1).Height,infoImage(1).Width,size(infoImage,1)));
-    for nZ = 1:size(infoImage,1)
-        loadedImage(:,:,nZ) = imread(fileName,nZ);
-    end
-end
--- 2.Read&WriteTiff/writeStackTiff.m
+++
@@ -1,8 +0,0 @@
-function writeStackTif(img,fileName)
-
-%% Write a Tiff file, appending each image as a new page
-    for ii = 1 : size(img, 3)
-        imwrite(img(:,:,ii) ,fileName, 'WriteMode' , 'append') ;
-    end
-
-end
