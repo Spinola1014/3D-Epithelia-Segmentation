8e7a33cefbeb398e2c47314524d8ffa96196f0db
Miguel Ángel Spínola <98082525+Spinola1014@users.noreply.github.com>
2022-08-25 11:45:46 +0200

    Update writeStackTiff.m

--- 2.Read&WriteTiff/writeStackTiff.m
+++ 2.Read&WriteTiff/writeStackTiff.m
@@ -1,6 +1,6 @@
 function writeStackTif(img,fileName)
 
-%write a Tiff file, appending each image as a new page
+%% Write a Tiff file, appending each image as a new page
     for ii = 1 : size(img, 3)
         imwrite(img(:,:,ii) ,fileName, 'WriteMode' , 'append') ;
     end
